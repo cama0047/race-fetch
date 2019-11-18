ce7321b2a8f081d1c68c5d389c4bfa82333f5828
Camilo CV <cama0047@algonquinlive.com>
2019-11-18 15:43:00 -0500

    new links

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-const url1 = 'https://jsonbox.io/box_7ebd8cb0b8fb187873ef'
-const url2 = 'https://jsonbox.io/box_82b5a4aa7a4a71abaa80'
+let datafile1 = fetch('https://gist.githubusercontent.com/prof3ssorSt3v3/22eb7745efdee9e130ed13cc19d85b75/raw/9dd9becedcf05e4392323645f7d8618ff26b1c38/box_7ebd8cb0b8fb187873ef.json');
+let datafile2 = fetch('https://gist.githubusercontent.com/prof3ssorSt3v3/9fcda9dc6d5e94e5689de33b0d9d45f8/raw/f1a89ab08aff538c1a7d4b85d0d7ba76ea92b88e/box_82b5a4aa7a4a71abaa80.json');
 
-
-
-let log = console.log;
-
-let datafile1 = fetch('https://jsonbox.io/box_7ebd8cb0b8fb187873ef');
-let datafile2 = fetch('https://jsonbox.io/box_82b5a4aa7a4a71abaa80');
-
-Promise.all([datafile1, datafile2])
+Promise.race([datafile1, datafile2])
     // .then(response => console.log(response))
-    .then(files => {
-        files.forEach(file => {
-            process(file.json());
-        })
-        // file[0].json()
-        // file[1].json()
+    .then({
 
     })
     .catch(err => console.log(err));
